a587a6e8b33936cff7e2febc8922156fba66bfa0
n.xarlov.2020 <n.xarlov.2020@CLOUD-RDHOST1>
2023-04-21 13:51:05 +0700

    Добавьте файлы проекта.

---
+++ Builder/BuilderFlyout.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Builder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BuilderFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public BuilderFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new BuilderFlyoutViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class BuilderFlyoutViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<BuilderFlyoutMenuItem> MenuItems { get; set; }
+
+            public BuilderFlyoutViewModel()
+            {
+                MenuItems = new ObservableCollection<BuilderFlyoutMenuItem>(new[]
+                {
+                    new BuilderFlyoutMenuItem { Id = 0, Title = "Page 1" },
+                    new BuilderFlyoutMenuItem { Id = 1, Title = "Page 2" },
+                    new BuilderFlyoutMenuItem { Id = 2, Title = "Page 3" },
+                    new BuilderFlyoutMenuItem { Id = 3, Title = "Page 4" },
+                    new BuilderFlyoutMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ Builder/BuilderFlyoutMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class BuilderFlyoutMenuItem
+    {
+        public BuilderFlyoutMenuItem()
+        {
+            TargetType = typeof(BuilderFlyoutMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
